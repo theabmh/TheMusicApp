69774a18f819586848f7c672bcd28279bfbd7b98
Abbasmh <abbasabu3g@gmail.com>
2021-08-03 12:36:42 +0530

    Initial Commit

---
+++ TheMusicApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TheMusicApp
+//
+//  Created by abbas m h on 03/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TheMusicApp/TheMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMusicAppApp.swift
+//  TheMusicApp
+//
+//  Created by abbas m h on 03/08/21.
+//
+
+import SwiftUI
+
+@main
+struct TheMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
